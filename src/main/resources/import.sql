81db291a0ebaaeeadcea9d9fa85b2f711d962a1d
1mmanuelKant <joselisboaa05@gmail.com>
2023-07-19 21:27:28 -0300

    feat: importing data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO kitchen VALUES ("Brasileira");
+INSERT INTO kitchen VALUES ("Americana");
+INSERT INTO kitchen VALUES ("Chinesa");
+INSERT INTO kitchen VALUES ("Australiana");
+INSERT INTO kitchen VALUES ("Japonesa");
+INSERT INTO kitchen VALUES ("Tailandesa");
